961656ef470ce16d76be1d937a896aa9eed9244b
fortunate-partial-4781 <niko876@hotmail.com>
2019-03-07 23:13:35 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -10,3 +10,6 @@ def first_element(first_element)
   ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"][0]
 end
 
+def third_element(third_element)
+  ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"][2]
+end
